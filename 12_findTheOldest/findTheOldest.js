06da417590575039702fb98e016c6ac573b38b49
oualid <tridioualid@gmail.com>
2022-06-17 00:13:46 +0100

    Solve Exercice 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    return people.reduce((theOldest, person) =>{
+        if(theOldest.hasOwnProperty('yearOfDeath') && person.hasOwnProperty('yearOfDeath')){
+            if(
+                (theOldest.yearOfDeath - theOldest.yearOfBirth) < 
+                (person.yearOfDeath - person.yearOfBirth)
+            ) theOldest = person;
+        }else if(!theOldest.hasOwnProperty('yearOfDeath')){
+             if(
+                ((new Date()).getFullYear() - theOldest.yearOfBirth) < 
+                (person.yearOfDeath - person.yearOfBirth)
+            ) theOldest = person;
+        }else if(!person.hasOwnProperty('yearOfDeath')){
+             if(
+                (theOldest.yearOfDeath - theOldest.yearOfBirth) < 
+                ((new Date()).getFullYear() - person.yearOfBirth)
+            ) theOldest = person;
+        }
+
+
+        return theOldest;
+
+    }, people[0]);
+
+    
 };
 
 // Do not edit below this line
