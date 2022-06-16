79882f103a00e3dba31df315e98514b1f811228f
oualid <tridioualid@gmail.com>
2022-06-16 16:30:19 +0100

    Solve Exercice 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const strArray = str.toLowerCase().split('');
+    const specialCar = ['!',',','.',' '];
+    for(let i = specialCar.length ; i >= 0 ; i--){   
+        let index = strArray.indexOf(specialCar[i]);
+        while(index !== -1){
+            strArray.splice(index, 1);
+            index = strArray.indexOf(specialCar[i]);
+        }
+    }
+
+    if(strArray.join('') === strArray.reverse().join('')) return true;
+    else return false;
 };
 
 // Do not edit below this line
