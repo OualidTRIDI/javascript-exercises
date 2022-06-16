cf98975e5581b535dfe2e01cbf3de53cc4d7a705
oualid <tridioualid@gmail.com>
2022-06-16 18:24:13 +0100

    Solve Exercice 09

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(nbr) {
+
+    let Fn0 = 0;
+    let Fn1 = 1;
+    if(nbr < 0) return "OOPS";
+    else if(nbr == 0) return 0;
+    else if(nbr == 1) return 1;
+    else{
+        Fn1 = fibonacci(nbr - 1);
+        Fn0 = fibonacci(nbr - 2);
+        return Fn0 + Fn1;
+    }
 
 };
 
