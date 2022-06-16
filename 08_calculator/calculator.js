78e8e1c679fbd3bed158a61655552b3a6361e6e4
oualid <tridioualid@gmail.com>
2022-06-16 15:06:34 +0100

    Solve Calculator Exercice

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(nbr1, nbr2) {
+	return nbr1 + nbr2;
 };
 
-const subtract = function() {
-	
+const subtract = function(nbr1, nbr2) {
+	return nbr1 - nbr2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(function (total, currentValue) {
+    return total + currentValue
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(function(multiplies, currentValue){
+    return multiplies * currentValue;
+  },1);
 };
 
-const power = function() {
-	
+const power = function(nbr1, nbr2) {
+	return Math.pow(nbr1, nbr2);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number === 0) return 1;
+  else if( number === 1) return 1;
+  else{
+    let fact = 1;
+    for(let i = number ; i > 1 ; i--){
+      fact = fact * i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
